9c7ccc55e769d96eeeabfded2c54b525b5cb785b
shimba <vitaly63fomin@gmail.com>
2016-12-20 16:59:24 +0400

    Upload project

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # mask-input
+Simple module that provides a mask to enter field.
+### How to use? ###
+```javascript
+document.querySelectorAll('input[data-mask]').forEach(function(val, key) {
+    new Mask(val);
+});
+```
+```html
+<input type="text" value="" data-mask="+7 (___) - ___ - __ - __"/>
+```
+### Options ###
+```javascript
+new Mask(document.querySelector('input[data-mask]'), {
+    prefix: '_',
+    keyIgnore: [8,37,39,46],
+    callback: {
+        success: function (input) {
+            console.log('success');
+        },
+        error: function (input) {
+            console.log('error');
+        }
+    }
+});
+```
+
+#### Available Options ####
+
+* __setPlaceholder__: Set placeholder for input.
+* __prefix__: Template for characters.
+* __keyIgnore__: Keys to avoid.
+* __callback.success__: Callback to execute when the input verification success.
+* __callback.error__: Callback to execute when the input verification error.
---
+++ mask.js
@@ -0,0 +1,128 @@
+(function (global, factory) {
+    'use strict';
+
+    if (typeof define === 'function' && define.amd) {/* Use AMD */
+        define(factory());
+    } else if (typeof module !== 'undefined' && module.exports) {/* Use CommonJS */
+        module.exports = factory();
+    } else {/* Use Browser */
+        global.Mask = factory();
+    }
+})(typeof window !== 'undefined' ? window : this, function () {
+    var Mask = function (_input, option) {
+        var self = this;
+
+        var _data = {},
+            _option = {
+                prefix: '_',
+                keyIgnore: [8,37,39,46],
+                setPlaceholder: true,
+                callback: {
+                    success: function (input) {},
+                    error: function (input) {}
+                }
+            },
+            _mask = null,
+            _splitMask = [],
+            _replaceNum = null,
+            _currentVal = null,
+            _cursorPosition = null;
+
+        this.init = function () {
+            if (_input === undefined || _input.length > 1) return false;
+
+            _option = Object.assign({}, _option, option);
+
+            _data = _input.dataset;
+            _mask = _data.mask;
+            _splitMask = _mask.split(_option.prefix);
+            _replaceNum = _mask.trim().replace(/[^\d]/gi, '');
+            _currentVal = _input.value;
+            _cursorPosition = 0;
+
+            if (_option.setPlaceholder) {
+                _input.placeholder = _mask;
+            }
+
+            _input.addEventListener('keyup', function(event) {
+                self.setVal(this, event);
+            });
+        };
+
+        this.getMaskedVal = function (value) {
+            var clearVal = value.trim().replace(/[^\d]/gi, ''),
+                splitVal = clearVal.split(''),
+                maskedVal = _splitMask.slice(0);
+
+            if (clearVal.indexOf(_replaceNum) != -1) {
+                splitVal = splitVal.slice(_replaceNum.split('').length);
+            }
+
+            maskedVal.forEach(function (val, key) {
+                if (key == (maskedVal.length - 1) || maskedVal[key] == undefined) return false;
+
+                if (splitVal[key] == undefined) {
+                    maskedVal[key] = maskedVal[key] + _option.prefix;
+                } else {
+                    maskedVal[key] = maskedVal[key] + splitVal[key];
+                    self.setCursorPosition(maskedVal, key);
+                }
+            });
+
+            return maskedVal.join('');
+        };
+
+        this.setCursorPosition = function (maskedVal, key) {
+            var arr = [];
+
+            for (var i = 0; i <= key; i++) {
+                arr[i] = maskedVal[i];
+            }
+
+            _cursorPosition = arr.join('').length;
+        };
+
+        this.setCursor = function (input, charPos) {
+            if(input != null && input != undefined) {
+                if(input.createTextRange) {
+                    var range = input.createTextRange();
+
+                    range.move('character', charPos);
+                    range.select();
+                } else {
+                    if(input.selectionStart) {
+                        input.focus();
+                        input.setSelectionRange(charPos, charPos);
+                    } else {
+                        input.focus();
+                    }
+                }
+            }
+        };
+
+        this.check = function (input) {
+            if (input.value.indexOf(_option.prefix) == -1) {
+                _option.callback.success(input);
+            } else {
+                _option.callback.error(input);
+            }
+        };
+
+        this.setVal = function (input, event) {
+            _currentVal = input.value;
+            _cursorPosition = input.selectionStart;
+
+            var value = self.getMaskedVal(_currentVal);
+
+            if (_currentVal != value && (event == null || _option.keyIgnore.join(',').indexOf(event.keyCode) < 0)) {
+                input.value = value;
+                self.setCursor(input, _cursorPosition);
+                self.check(input);
+            }
+        };
+
+        this.init();
+    };
+
+    return Mask;
+});
